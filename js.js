b3c8ebaef580278c40b61bf40179024a6543a117
StructML <struct.machine.learning@gmail.com>
2016-07-09 17:14:49 -0400

    time fix

--- js.js
+++ js.js
@@ -1,5 +1,16 @@
 var overlay = document.getElementById("overlay");
 
-window.addEventListener('load', function(){
+var startTime = new Date().getTime();
+
+var interval = setInterval(function(){
+    if(new Date().getTime() - startTime > 60000){
+        clearInterval(interval);
+        return;
+    }
+    window.addEventListener('load', function(){
   overlay.style.display = 'none';
+  
+  
 })
+}, 2000);     
+
