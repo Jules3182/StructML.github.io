e8968f051a74ea174a6825042fc6feeec7ed0967
StructML <struct.machine.learning@gmail.com>
2016-07-09 17:18:58 -0400

    Update js.js

--- js.js
+++ js.js
@@ -1,12 +1,5 @@
 var overlay = document.getElementById("overlay");
 
-var startTime = new Date().getTime();
-
-var interval = setInterval(function(){
-    if(new Date().getTime() - startTime > 20){
-        clearInterval(interval);
-        return;
-    }
     window.addEventListener('load', function(){
   overlay.style.display = 'none';
   
